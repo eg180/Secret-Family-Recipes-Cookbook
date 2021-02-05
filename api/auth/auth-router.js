4b8de6a9ba66c35379809b3d48894f0410220f9a
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-04 22:18:19 -0500

    token working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Users = require("../resources/users/users-model.js");
+const jwt = require('jsonwebtoken');
 // const bcrypt = require("bcryptjs");
 
 
@@ -11,14 +12,34 @@ router.post("/login", async (req, res) => {
         console.log('here is info destructured from the req bod')
         console.log(user_username)
         console.log(user_password)
+        console.log('before await')
         const allegedUser = await Users.findBy({ user_username })
+        console.log('after await')
+        console.log(allegedUser)
         // res.json('no error, but look at res')
+        const token = makeJwt(allegedUser);
 
-        res.status(200).json(allegedUser)
+        res.status(200).json({allegedUser, token})
         
     } catch (err) {
         res.status(500).json(err.message)
     }
 });
+
+function makeJwt(user) {
+    const payload = {
+        subject: user.user_id,
+        username: user.user_username,
+        role: user.user_role
+    };
+
+    const secret = process.env.JWT_SECRET || 'shh'
+
+    const options = {
+        expiresIn: "15000"
+    }
+
+    return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
